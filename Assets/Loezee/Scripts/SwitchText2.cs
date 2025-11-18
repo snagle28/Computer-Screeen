05d3d4bfc48991f15d9437777e6327b7ef82cfa0
snagle28 <san7579@nyu.edu>
2025-11-18 16:01:58 -0500

    fixing switch text

--- Assets/Loezee/Scripts/SwitchText2.cs
+++ Assets/Loezee/Scripts/SwitchText2.cs
@@ -1,42 +1,57 @@
 using UnityEngine;
+using UnityEngine.UI;
+/*
+ * In order to set this up: have two button objects with this script attached.
+ * Assign each other as partners in the inspector.
+ * Assign the display images to each button (one active, one inactive).
+ * Assign one button as the controller.
+ */
 
 public class SwitchText2 : MonoBehaviour
 {
     //assign a partner to each text
-    public SwitchText2 partner;  
+    public SwitchText2 partner;
 
-    [Header("Images for this pair")]
-    public SpriteRenderer image4;   
-    public SpriteRenderer image5;   
+    [Header("Images for this pair")] 
+    public GameObject displayImage1;
+    public GameObject displayImage2; //starts inactive
 
-    private SpriteRenderer sr;
+    public bool isController;
+
+    private Button button;
     private bool isSelected = false;
+
+    public bool puzzleDone = false;
 
     //static = shared by the pair so image only switches once
     private static bool imageSwitched = false;
 
     void Awake()
     {
-        sr = GetComponent<SpriteRenderer>();
+        button = GetComponent<Button>();
     }
+    //
+    // void Update()
+    // {
+    //     //swap when 2 are selected and enter is pressed
+    //     if (isSelected && partner.isSelected)
+    //     {
+    //         if (Input.GetKeyDown(KeyCode.Return))
+    //         {
+    //             SwapPositions();
+    //         }
+    //     }
+    // }
 
-    void Update()
-    {
-        //swap when 2 are selected and enter is pressed
-        if (isSelected && partner.isSelected)
-        {
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                SwapPositions();
-            }
-        }
-    }
-
-    void OnMouseDown()
+    public void OnClick()
     {
         // toggle selection state
         isSelected = !isSelected;
         SetSelected();
+        if (isSelected && partner != null && partner.isSelected)
+        {
+            SwapPositions();
+        }
     }
 
     private void SwapPositions()
@@ -54,25 +69,38 @@ public class SwitchText2 : MonoBehaviour
         SetSelected();
         partner.SetSelected();
 
-        //just switch imageB1 and imageB2 once
-        if (!imageSwitched && image4 != null && image5 != null)
+         // Swap the visible sprite between the two display Image components (only once if desired)
+        if (!imageSwitched && displayImage1 != null && displayImage2 != null && isController)
         {
-            image4.enabled = false;
-            image5.enabled = true;
-            imageSwitched = true;
+            if (displayImage1.activeSelf)
+            {
+                displayImage1.SetActive(false);
+            }
+            else
+            {
+                displayImage1.SetActive(true);
+            }
+
+            if (!displayImage2.activeSelf)
+            {
+                displayImage2.SetActive(true);
+            }
+            else
+            {
+                displayImage2.SetActive(false);
+            }
+           // imageSwitched = true;
+           puzzleDone = true;
         }
     }
 
     private void SetSelected()
     {
-        //yellow for highlights, white for unselected
+        // Highlight yellow when selected, white when not
+        Image img = button.GetComponent<Image>();
         if (isSelected)
-        {
-            sr.color = new Color(1f, 1f, 0.6f, 1f);  
-        }
+            img.color = new Color(1f, 1f, 0.6f, 1f);
         else
-        {
-            sr.color = Color.white;    
-        }
+            img.color = Color.white;
     }
 }
