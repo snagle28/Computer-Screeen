6a56f9624d7a4afae863aac6a455425ce8a18bcd
Loezee <ll5669@nyu.edu>
2025-12-11 04:35:31 -0500

    animation fix

--- Assets/Loezee/Scripts/ErrorAnimation.cs
+++ Assets/Loezee/Scripts/ErrorAnimation.cs
@@ -3,16 +3,23 @@ using UnityEngine.UI;
 
 public class errorAnimation : MonoBehaviour
 {
-    public Animator animator;
-    public string triggerName = "PlayErrorAnimation";
+    public Animator animator;  
+    public string triggerName = "playError";  
 
-    [SerializeField] private Button page10button;
+    [SerializeField] 
+    private Button page10button;  
+    [SerializeField]
+     private GameObject page10;    
+    [SerializeField] 
+    private GameObject page11;   
+     
+    private bool animationPlaying = false;
 
     void Start()
     {
         if (page10button != null)
         {
-            page10button.onClick.AddListener(PlayAnimation);
+            page10button.onClick.AddListener(PlayAnimation);  
         }
 
         if (animator == null)
@@ -20,14 +27,39 @@ public class errorAnimation : MonoBehaviour
             animator = GetComponent<Animator>();
         }
 
-
         animator.enabled = false; 
     }
 
+    void Update()
+    {
+        //checking to see if animation has ended
+        if (animationPlaying)
+        {
+            //what's the current animation state
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+            //is the animation finished? if >= 1f, then yes
+            if (stateInfo.IsName("errorclip") && stateInfo.normalizedTime >= 1.0f)
+            {
+                animationPlaying = false;  //stop checking
+
+                //go from page10 to page11
+                SwitchPages();
+            }
+        }
+    }
 
     void PlayAnimation()
     {
-        animator.enabled = true;
-        animator.SetTrigger(triggerName);
+        animator.enabled = true;  
+        animator.SetTrigger(triggerName);  
+        animationPlaying = true;  //start monitoring the animation
+    }
+
+    //switch from page10-11
+    void SwitchPages()
+    {
+        page10.SetActive(false);  //disable page10
+        page11.SetActive(true);   //activate page11
     }
 }
